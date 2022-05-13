04319349ff0a70de9db61895849e779ee519d982
Alexander Sokolov <59219077+himawariq@users.noreply.github.com>
2022-05-13 23:38:07 +0300

    directory

--- app.py
+++ app.py
@@ -13,9 +13,8 @@ import numpy as np
 import torch
 import torch.nn as nn
 import streamlit as st
-from googleDriveFileDownloader import googleDriveFileDownloader
 
-gdownloader = googleDriveFileDownloader()
+
 
 st.set_page_config(
      page_title="SokolovAVapp",
@@ -83,7 +82,7 @@ def generate_interpolated_images(
 
 
 def load_model(file_name: str, device: torch.device) -> nn.Module:
-    path = r'stylegan_human_v2_1024.pkl'
+    path = './models/stylegan_human_v2_1024.pkl'
     with open(path, 'rb') as f:
         model = pickle.load(f)['G_ema']
     model.eval()
@@ -95,7 +94,7 @@ def load_model(file_name: str, device: torch.device) -> nn.Module:
     return model
 
 def load_model1(file_name: str, device: torch.device) -> nn.Module:
-    path = r'network-snapshot-000560.pkl'
+    path = './models/network-snapshot-000560.pkl'
     with open(path, 'rb') as f:
         model = pickle.load(f)['G_ema']
     model.eval()
@@ -158,7 +157,7 @@ def main():
         args = parse_args()
         device = torch.device(args.device)
 
-        model = load_model('stylegan_human_v2_1024.pkl', device)
+        model = load_model('models/stylegan_human_v2_1024.pkl', device)
 
         func = functools.partial(generate_image, model=model, device=device)
         func = functools.update_wrapper(func, generate_image)
@@ -175,7 +174,7 @@ def main():
         args = parse_args()
         device = torch.device(args.device)
 
-        model = load_model('stylegan_human_v2_1024.pkl', device)
+        model = load_model('models/stylegan_human_v2_1024.pkl', device)
         func = functools.partial(generate_interpolated_images,
                                  model=model,
                                  device=device)
@@ -201,7 +200,7 @@ def main():
         args = parse_args()
         device = torch.device(args.device)
 
-        model = load_model1('network-snapshot-000560.pkl', device)
+        model = load_model1('models/network-snapshot-000560.pkl', device)
 
         func = functools.partial(generate_image, model=model, device=device)
         func = functools.update_wrapper(func, generate_image)
@@ -218,7 +217,7 @@ def main():
         args = parse_args()
         device = torch.device(args.device)
 
-        model = load_model1('network-snapshot-000560.pkl', device)
+        model = load_model1('models/network-snapshot-000560.pkl', device)
         func = functools.partial(generate_interpolated_images,
                                  model=model,
                                  device=device)
